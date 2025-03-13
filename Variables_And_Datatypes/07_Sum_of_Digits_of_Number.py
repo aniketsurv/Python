ca807b192f0360bbe22970e434e3a80e1ca81e6e
Aniket#0903 <aniketsurv>
2025-03-13 23:56:01 +0530

    s

--- Variables_And_Datatypes/07_Sum_of_Digits_of_Number.py
+++ Variables_And_Datatypes/07_Sum_of_Digits_of_Number.py
@@ -1,5 +1,13 @@
-a= 30
-b=50
+'''you simply add up each individual digit of that number. 
+   For example:
+   For the number 123, the sum of digits is:  1+2+3=6
+'''
 
-sum = a+b
-print(sum)
+num = 54321
+total = 0
+
+for i in range(len(str(num))):
+    print("index : ",i)
+    total += int(str(num)[i])
+
+print(f"Sum of digits : {total}")
---
+++ Variables_And_Datatypes/12_Area_of_Circle.py
@@ -0,0 +1,14 @@
+import math
+
+class area_of_circle:
+    
+    # 1st way
+    radius = 4
+    arc = 2.14 *radius*radius
+    print("area of circle",arc)
+
+    # 2nd way
+    radiusss = int(input("Enter a number : "))
+
+    arccc = math.pi * radiusss ** 2
+    print("area of circle",arc)
